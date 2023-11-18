bcdc5b12b8833bb49ae7f45a097b9343dec3b48b
alejandrojimenezvalero <100454273@alumnos.uc3m.es>
2023-11-18 23:07:01 +0100

    create block unit test added

---
+++ utest/block_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "../sim/block.hpp"
+
+TEST(CreateBlockTest, InitializesBlockCorrectly) {
+  int i = 1, j = 2, k = 3;
+  Block result = createBlock(i, j, k);
+
+  EXPECT_EQ(result.block_index[0], i);
+  EXPECT_EQ(result.block_index[1], j);
+  EXPECT_EQ(result.block_index[2], k);
+}
